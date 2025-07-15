d3c03480db809bd8aa1e5a237b168cf6e92a7b4e
solve-coagulate <ben.burns@gmail.com>
2025-07-16 05:57:47 +0800

    Split evaluator into modules

---
+++ lispfun/list_utils.lisp
@@ -0,0 +1,23 @@
+(define null?
+  (lambda (x)
+    (= x (quote ()))) )
+
+(define length
+  (lambda (lst)
+    (if (null? lst)
+        0
+        (+ 1 (length (cdr lst))))))
+
+(define map
+  (lambda (f lst)
+    (if (null? lst)
+        (quote ())
+        (cons (f (car lst)) (map f (cdr lst))))) )
+
+(define filter
+  (lambda (pred lst)
+    (if (null? lst)
+        (quote ())
+        (if (pred (car lst))
+            (cons (car lst) (filter pred (cdr lst)))
+            (filter pred (cdr lst))))) )
