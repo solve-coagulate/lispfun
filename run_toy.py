2e62c8192f9ea2bdafb12db5f62397e258e2762c
solve-coagulate <ben.burns@gmail.com>
2025-07-16 21:30:02 +0800

    Run toy script uses toy repl

--- run_toy.py
+++ run_toy.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 """Run the toy interpreter implemented in Lisp."""
 import sys
+import os
 from lispfun.interpreter import standard_env
-from lispfun.run import load_eval, load_toy, toy_run_file, repl
+from lispfun.run import load_eval, load_toy, toy_run_file
+
+TOY_REPL_FILE = os.path.join(os.path.dirname(__file__), "examples", "toy-repl.lisp")
 
 
 def main() -> None:
@@ -12,7 +15,7 @@ def main() -> None:
     if len(sys.argv) > 1:
         toy_run_file(sys.argv[1], env)
     else:
-        repl(env)
+        toy_run_file(TOY_REPL_FILE, env)
 
 
 if __name__ == "__main__":
