7c7b506c79cdfa53d3f80715b6ba7c413d2200bc
solve-coagulate <ben.burns@gmail.com>
2025-07-16 08:23:10 +0800

    Refactor toy interpreter into modules

---
+++ examples/toy-parser.lisp
@@ -0,0 +1,24 @@
+(import "examples/toy-tokenizer.lisp")
+
+; Parse tokens into an expression
+(define read-from-tokens
+  (lambda (tokens)
+    (if (null? tokens)
+        (list (quote ()) (quote ()))
+        (let ((token (car tokens)))
+          (set! tokens (cdr tokens))
+          (cond
+            ((= token "(")
+             (define loop
+               (lambda (ts acc)
+                 (if (= (car ts) ")")
+                     (list (reverse acc) (cdr ts))
+                     (let ((res (read-from-tokens ts)))
+                       (loop (cadr res) (cons (car res) acc)))))
+             (loop tokens (quote ())))
+            ((= token ")") (list (quote error) tokens))
+            (else (list token tokens))))))
+
+(define parse
+  (lambda (text)
+    (car (read-from-tokens (tokenize text)))) )
